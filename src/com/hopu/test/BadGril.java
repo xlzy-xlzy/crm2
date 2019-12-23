34cf5406e3ae8a99d45ab0ea7df587b7dce0972f
xlzy-xlzy <15188275064@163.com>
2019-12-23 20:12:09 +0800

    修改源码4去掉数据

--- src/com/hopu/test/BadGril.java
+++ src/com/hopu/test/BadGril.java
@@ -6,8 +6,7 @@ public class BadGril {
 		// TODO Auto-generated method stub
 		System.out.println("you father");
 		System.out.println("123456789");
-		System.out.println("nmsl");
-		System.out.println("nbml");
+		
 	}
 
 }
